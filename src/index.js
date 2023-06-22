1f4320dedc1bf2c67417ce04ceb12084330ac1a0
Bavaturka <45878322+Bavaturka@users.noreply.github.com>
2023-06-22 15:03:57 +0200

    second

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import './index.css';
+import Home from './components/Home';
+import About from  './components/About';
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className='site-logo' to='/'>#VanLife</Link>
+        <nav>
+          <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
